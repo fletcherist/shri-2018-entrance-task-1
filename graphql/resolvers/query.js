116f0e593c8ce4a9a336a7f80ebe389e64b63acc
fletcherist <romanovexports@gmail.com>
2017-12-15 22:57:51 +0300

    [fix]: sqlite3 initialization

--- graphql/resolvers/query.js
+++ graphql/resolvers/query.js
@@ -1,19 +1,20 @@
 const { models } = require('../../models');
 
 module.exports = {
-event (root, { id }) {
+  event (root, { id }) {
     return models.Event.findById(id);
   },
+  /* @bug seems to be bug here */
   events (root, args, context) {
     return models.Event.findAll(argumets, context);
   },
-user (root, { id }) {
+  user (root, { id }) {
     return models.User.findById(id);
   },
   users (root, args, context) {
     return models.User.findAll({}, context);
   },
-room (root, { id }) {
+  room (root, { id }) {
     return models.Room.findById(id);
   },
   rooms (root, args, context) {
--- models/index.js
+++ models/index.js
@@ -4,14 +4,24 @@ const scheme = require('./scheme');
 
 const Op = Sequelize.Op;
 
-const sequelize = new Sequelize(null, null, {
+const sequelize = new Sequelize('database', null, null, {
   dialect: 'sqlite',
   storage: 'db.sqlite3',
 
-operatorsAliases: { $and: Op.and },
-
+  operatorsAliases: { $and: Op.and },
   logging: false
 });
+
+// const sequelize = new Sequelize('sqlite:db.sqlite3')
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 scheme(sequelize);
 sequelize.sync();
